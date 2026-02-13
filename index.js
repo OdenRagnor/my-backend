cf82fa3923fe6a8c4ab763ea44003338a1022151
OdenRagnor <maher4412@gmail.com>
2026-02-13 12:58:31 -0500

    added HTML to index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require("path");
 
 app.use(express.json());
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('API is running');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
